34adf6cf7c890e8baf46b09887c864e60312af2f
table106 <cytrynek106@gmail.com>
2025-01-16 12:36:10 +0100

    fix strings

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import asyncio
-import datetime
+from datetime import date
 import python_weather as pyweather
 
 async def getWeather(area: str, /) -> None:
@@ -7,7 +7,7 @@ async def getWeather(area: str, /) -> None:
         task: asyncio.Task = asyncio.create_task(client.get(area))
         try:
             forecast = await asyncio.wait_for(task, timeout=5)
-            sunnyHours: dict[datetime.date, list[int]] = {}
+            sunnyHours: dict[date, list[int]] = {}
             for dayForecast in forecast:
                 sunnyHours[dayForecast.date] = []
                 todaysSun = sunnyHours[dayForecast.date]
@@ -16,15 +16,20 @@ async def getWeather(area: str, /) -> None:
                         todaysSun.append(hourForecast.time.hour)
                 if todaysSun != []:
                     todaysSun.append(dayForecast.moon_phase)
-            
+
             for k,v in sunnyHours.items():
                 if v != []:
-                    print(f"On {k.day}/{k.month}, the sky will be clear during the hours: {", ".join([str(n) for n in v if type(n) == int])}. The moon phase will be {[n for n in v if type(n) == pyweather.Phase][0]}")
-                    print("(Displaying forecast for today and 2 days into the future)")
+                    print(f"On {k.day}/{k.month}, the sky will be clear during the hours: ")
+                    print(f"{", ".join([str(n) for n in v if type(n) == int])}.")
+                    print("The moon phase will be ", end="")
+                    print(f"{[n for n in v if type(n) == pyweather.Phase][0]}.")
+                    print()
+            print("(Displaying forecast for today and 2 days into the future)")
+            input("Press enter to exit")
         except Exception as err:
             print("An error occured. Please try again.")
             print(err)
 
 if __name__ == "__main__":
-    area: str = input("What area do you want the weather of? (Country, City, etc.)\n>")
-    asyncio.run(getWeather(area))
+    area: str = input("What area do you want the weather of? (country, city, etc.)\n>")
+    asyncio.run(main=getWeather(area))
